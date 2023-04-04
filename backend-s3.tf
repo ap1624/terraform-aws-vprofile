c60dd257a518c4679c143614a8158c2bbf49d773
Abhi <ap1624@gmail.com>
2023-04-04 18:30:17 +0530

    key pairs

--- backend-s3.tf
+++ backend-s3.tf
@@ -1,7 +1,7 @@
 terraform {
-    backend "s3" {
-     bucket = "terra-vprofile-state15"
-     key = "terraform/backend"
-     region = "us-east-2"
-     }
- }
+  backend "s3" {
+    bucket = "terra-vprofile-state15"
+    key    = "terraform/backend"
+    region = "us-east-2"
+  }
+}
